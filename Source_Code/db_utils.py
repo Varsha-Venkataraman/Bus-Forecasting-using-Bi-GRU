86ce48d34918b5983d09b4db29f9368f27fbb8dc
Varsha Venkataraman <varshav19305@gmail.com>
2026-05-22 17:55:17 +0530

    db_utils.py

---
+++ Source_Code/db_utils.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+def sql_connect():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="<username>",
+            password="<password>",
+            database="vehicle_mgt"
+        )
+        cursor = conn.cursor(dictionary=True)
+        print("SQL Connected")
+        return conn, cursor
+    except mysql.connector.Error as e:
+        print(f"Database connection error: {e}")
+        return None, None
+
+def sql_disconnect(conn, cursor):
+    if cursor:
+        cursor.close()
+    if conn and conn.is_connected():
+        conn.close()
+        print("SQL Disconnected\n\n")
